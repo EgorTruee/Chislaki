d7221663edac273f1ac488b1e151c79b8e1f2f00
Egor <trusov.egor@gmail.com>
2024-03-20 01:19:19 +0300

    N15

---
+++ N15/N15.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+double Integral(vector<double> x, vector<double> u)
+{
+	double I = 0;
+	double F = u[0];
+
+	for (int i = 1; i < u.size(); i++)
+	{
+		I += (F + u[i]) / 2 * (x[i] - x[i - 1]);
+		F = u[i];
+	}
+	return I;
+}
+function<double(double)> Phi(unsigned int k)
+{
+	return [k](double x) {return pow(x, k); };
+}
+vector<double> Gaus(vector<vector<double>> a, vector<double> b)
+{
+	int N = a.size();
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			double c = a[j][i] / a[i][i];
+
+			b[j] -= c * b[i];	
+
+			for (int k = i + 1; k < N; k++)
+			{
+				a[j][k] -= c * a[i][k];
+			}
+		}
+	}
+	vector<double> res(N);
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		double Sum = 0;
+
+		for (int j = i + 1; j < N; j++)
+		{
+			Sum += a[i][j] * res[j];
+		}
+		res[i] = (b[i] - Sum) / a[i][i];
+	}
+	return res;
+}
+vector<double> PolynomApproximation(int k, vector<double> x, vector<double> u)
+{
+	vector<vector<double>> A(k, vector<double>(k));
+	vector<double> B(k);
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			vector<double> t(x.size());
+
+			for (int b = 0; b < x.size(); b++)
+			{
+				t[b] = Phi(i)(x[b]) * Phi(j)(x[b]);
+			}
+			
+			A[i][j] = Integral(x, t);
+		}
+		vector<double> UPhi(x.size());
+
+		for(int j = 0; j < x.size(); j++)
+		{
+			UPhi[j] = u[j] * Phi(i)(x[j]);
+		}
+		B[i] = Integral(x, UPhi);
+	}
+	return Gaus(A, B);
+}
+vector<double> Approximation(vector<double> x, vector<double> u, vector<double> delta)
+{
+	double SqrDelta = 0;
+	double k = 1;
+	double SqrError = 0;
+	vector<double> res;
+
+	for (auto& i : delta)
+	{
+		SqrDelta += i * i;
+	}
+	cout << SqrDelta << endl;
+
+	do
+	{
+		res = PolynomApproximation(k, x, u);
+		SqrError = 0;
+
+		for (int i = 0; i < x.size(); i++)
+		{
+			double F = 0;
+
+			for (int j = 0; j < res.size(); j++)
+			{
+				F += res[j] * Phi(j)(x[i]);
+			}
+			SqrError += (u[i] - F) * (u[i] - F);
+		}
+		k++;
+
+	} while (SqrError > SqrDelta);
+
+	return res;
+}
+int main()
+{
+	vector<double> res = Approximation({ 1, 4, 9, 16, 25}, { 1, 2, 3, 4, 5}, {1, 1, 1, 1, 1});
+	double s = 0;
+
+	for (auto i : res)
+	{
+		cout << i << endl;
+	}
+	cout << endl;
+
+	for (int i = 0; i < res.size(); i++)
+	{
+		s += res[i] * Phi(i)(2);
+	}
+	cout << s;
+}

38af9a3a1894cde93e9b6e786c6ce631be191df6
Egor <trusov.egor@gmail.com>
2024-03-26 08:44:54 +0300

    N10 and N11 pointer variant

--- N10/N10.cpp
+++ N10/N10.cpp
@@ -3,6 +3,37 @@
 
 using namespace std;
 
+void Gaus1(double** system, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			double c = system[j][i] / system[i][i];
+
+			for (int k = i + 1; k < n + 1; k++)
+			{
+				system[j][k] = system[j][k] - c * system[i][k];
+			}
+		}
+	}
+	vector<double> res(n);
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		double sum = 0;
+		
+		for (int j = i + 1; j < n; j++)
+		{
+			sum += system[i][j] * res[j];
+		}
+		res[i] = (system[i][n] - sum) / system[i][i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << res[i] << endl;
+	}
+}
 vector<double> Gause(vector<vector<double>> system)
 {
 	int n = system.size();
--- N11/N11.cpp
+++ N11/N11.cpp
@@ -3,6 +3,30 @@
 
 using namespace std;
 
+void Progonka1(double a[], double b[], double c[], double d[], int N)
+{
+
+	c[0] = -c[0] / b[0];
+	d[0] = d[0] / b[0];
+
+	for (int i = 1; i < N; i++)
+	{
+		c[i] = -c[i] / (b[i] + a[i] * c[i - 1]);
+		d[i] = (-a[i] * d[i - 1] + d[i]) / (b[i] + a[i] * c[i - 1]);
+	}
+	a[N - 1] = d[N - 1];
+
+	for (int i = N - 2; i >= 0; i--) 
+	{
+		a[i] = a[i + 1] * c[i] + d[i];
+	}
+	for (int i = 0; i < N; i++)
+	{
+		cout << a[i] << endl;
+	}
+	cout << endl;
+}
+
 vector<double> Progonka(vector<double> a, vector<double> b, vector<double> c, vector<double> d)
 {
 	int N = d.size();
@@ -37,6 +61,12 @@ int main()
 		{ 3, 4, 5 },
 		{ 6, 7, 0 },
 		{ 1, 2, 3 });
+	double a[] = { 0, 1, 2 };
+	double b[] = { 3, 4, 5 };
+	double c[] = { 6, 7, 0 };
+	double d[] = { 1, 2, 3 };
+	
+	Progonka1(a, b, c, d, 3);
 
 	for (auto& i : solution)
 	{
